8bb50feb1989bd591628b8acf98ee6cafd1b4297
nazrinkarimli8 <karimlinazrin8@gmail.com>
2024-05-08 18:42:31 +0400

    Booking Plane Tickets App

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,9 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+
+        System.out.println("helloooooo");
+        System.out.println("helloooooo");
         System.out.println("helloooooo");
     }
 }
